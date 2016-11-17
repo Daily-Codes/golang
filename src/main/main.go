97e07504535554fc996fa5d2392d74a425b5ad25
ZhongWen Li <lizhongwen1989@gmail.com>
2016-11-17 21:20:00 +0800

    Update

---
+++ src/main/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int
+	var b int8
+	var c int16
+	var d int32
+	var e int64
+
+	var f bool
+	var g float32
+	var h float64
+
+	var i string
+
+	var j [5] int
+
+	fmt.Println(a, "\t", b, "\t", c, "\t", d, "\t", e, "\t", f, "\t", g, "\t", h, "\t", i, "\t", j)
+
+}
