0180475f1770af9bb33d414412f342c64539ba44
ZhongWen Li <lizhongwen1989@gmail.com>
2016-11-17 22:10:19 +0800

    Update

--- src/main/main.go
+++ src/main/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"time"
 )
 
 func main() {
@@ -17,8 +19,39 @@ func main() {
 
 	var i string
 
-	var j [5] int
+	var j [5]int
+
+	var k uint
+	var l uint8
+	var m uint16
+	var n uint32
+	var o uint64
 
 	fmt.Println(a, "\t", b, "\t", c, "\t", d, "\t", e, "\t", f, "\t", g, "\t", h, "\t", i, "\t", j)
+
+	a = math.MinInt8
+	b = math.MinInt8
+	c = math.MinInt16
+	d = math.MinInt32
+	e = math.MinInt64
+
+	fmt.Println(a, "\t", b, "\t", c, "\t", d, "\t", e)
+
+	a = math.MaxInt8
+	b = math.MaxInt8
+	c = math.MaxInt16
+	d = math.MaxInt32
+	e = math.MaxInt64
+
+	k = math.MaxUint8
+	l = math.MaxUint8
+	m = math.MaxUint16
+	n = math.MaxUint32
+	o = math.MaxUint64
+
+	fmt.Println(a, "\t", b, "\t", c, "\t", d, "\t", e)
+	fmt.Println(k, "\t", l, "\t", m, "\t", n, "\t", o)
+
+	fmt.Println("Hello World, ", time.Now())
 
 }
